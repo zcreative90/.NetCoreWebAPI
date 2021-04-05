b93b4bbc3e9d57412c35b16d9b03343d7d4080bb
zcreative <80815841+zcreative90@users.noreply.github.com>
2021-04-05 11:16:14 +0300

    Add files via upload

---
+++ SehirRehberi.API/SehirRehberi.API/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using SehirRehberi.API.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SehirRehberi.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private DataContext _context;
+
+        // Burada Injection yaptık. Bu contexti Startup.cs de arar.
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetValues() {
+
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetValue(int id) 
+        {
+            var value = await _context.Values.FindAsync(id);
+
+            return Ok(value);
+        }
+        
+    }
+}
---
+++ SehirRehberi.API/SehirRehberi.API/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SehirRehberi.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SehirRehberi.API.Data
+{
+
+
+    // Modeldeki sınıfı veritabanındaki tabloya yansıtma
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
